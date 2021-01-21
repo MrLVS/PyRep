533d8c91af95fc2c9f79c03660acf40ed30cedd3
MrLVS <mrlvs2015@gmail.com>
2021-01-21 13:15:51 +0300

    1 file

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+# Pithon script
 
-#Pithon script
+a = True
+b = "True"
+c = 1
+x = a == c
+y = a == b
+
+print(type(a))  # <class 'bool'>
+print(type(b))  # <class 'str'>
+print(type(x))  # <class 'bool'>
+print(x)  # True  bool a соотвествует int c
+print(type(y))  # <class 'bool'>
+print(y)  # False
+
+name = input("Enter your name: ")
+print(name)
