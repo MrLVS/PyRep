1b6ee3c380d60c3b4477766899c293733c046812
MrLVS <mrlvs2015@gmail.com>
2021-01-22 19:51:19 +0300

    1 file

--- HW1.py
+++ HW1.py
@@ -3,16 +3,26 @@
 a = True
 b = "True"
 c = 1
+d = False
+e = 0
+f = "False"
 x = a == c
 y = a == b
+z = d == e
+w = f == d
+
+
 
 print(type(a))  # <class 'bool'>
 print(type(b))  # <class 'str'>
 print(type(x))  # <class 'bool'>
-print(x)  # True  bool a соотвествует int c
+print(x)  # True  bool a(True) соотвествует int c(1)
 print(type(y))  # <class 'bool'>
 print(y)  # False
+print(z) # True bool d(False) соответствует int e(0)
+print(w) # False
 
+# input demonstration
 name = input("Enter your name: ")
 print(name)
 print(type(name))
--- HW2.py
+++ HW2.py
@@ -1,7 +1,15 @@
 # input function
 
-
+list = []
 print("Введите любые слова, слоги или числа и разделите их пробелом.")
 text = input("--> ")
 
-print(text.split())
+splittext = text.split()
+
+for i in splittext:
+    if i not in list:
+        list.append(i)
+
+print(splittext)
+
+print(list)
