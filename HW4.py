ba104c43b2ebbb3ac7f79e234fb42223e308db85
MrLVS <mrlvs2015@gmail.com>
2021-01-24 00:53:38 +0300

    1 file

---
+++ HW4.py
@@ -0,0 +1,22 @@
+print("Введите любые целые неотрицательные числа, разделенные любым не цифровым литералом(пробел, запятая, буква")
+answer = input("--> ")
+
+
+def find_sum_int_in_string(inputstring):
+
+    sumnumbers = 0
+    stringelement = ""
+
+    for letter in inputstring:
+        if letter.isdigit():
+            stringelement += letter
+        elif stringelement != "":
+            sumnumbers += int(stringelement)
+            stringelement = ""
+    if stringelement != "": #Проверка последнего элемента, если он был числом
+        sumnumbers += int(stringelement)
+
+    print(sumnumbers)
+
+
+find_sum_int_in_string(answer)
