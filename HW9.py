da683a155db9d8d2372ccd0f0cdb9785c6b743b2
Vitaliy Lukin <mrlvs2015@gmail.com>
2021-02-11 14:40:00 +0300

    Проект Эйлера. Проблемы №№6,9,40,48

--- HW9.py
+++ HW9.py
@@ -1,6 +1,5 @@
 # ProjectEuler.ru,  problem solutions №№ 6, 9 , 40, 48
 
-import time
 from itertools import combinations
 import numpy
 
@@ -33,30 +32,7 @@ def pythagorean_triplet_list_comprehension():
     print(f"Тройки пифагора {triplet2}, произведение {result2} - list comprehension var 2\n")
 
 
-start_time = time.time()
 pythagorean_triplet_list_comprehension()
-end_time = time.time()
-print("Время выполнения", end_time - start_time)
-
-
-# problem #9
-# solution #2 more quickly that solution #1 Euclid formula (a=m ^ 2 - n ^ 2, b = 2mn, c = m ^ 2 + n ^ 2)
-def pythagorean_triplet_m_n():
-    """ Function for finding Pythagoras triples with a sum equal to 1000."""
-    for n in range(1, 32):
-        for m in range(1, 32):
-            a = n ** 2 - m ** 2
-            b = 2 * n * m
-            c = n ** 2 + m ** 2
-            if a + b + c == 1000:
-                return a * b * c
-
-
-start_time1 = time.time()
-print("Solution problem #9")
-print(pythagorean_triplet_m_n(), " - более быстрое решение через формулу Эйлера, но с применением циклов.")
-end_time1 = time.time()
-print("Время выполнения", end_time1 - start_time1, "\n")
 
 
 # Solution problem #40
@@ -65,7 +41,7 @@ print("Solution problem #40")
 print(list_d[1] * list_d[10] * list_d[100] * list_d[1000] * list_d[10000] * list_d[100000] * list_d[1000000], "\n")
 
 # Solution problem #48
-
-a = str(sum([pow(x, x) for x in range(1, 1001)]))
 print("Solution problem #48")
-print(a[-10:])
+print(str(sum([pow(x, x) for x in range(1, 1001)]))[-10:])
+
+
