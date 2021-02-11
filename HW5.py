0e5deccd28022023ce66a7d7f2f888bbc9a4a043
Vitaliy Lukin <mrlvs2015@gmail.com>
2021-02-11 15:20:22 +0300

    Функция для поиска наименьшего числа вне списка

--- HW5.py
+++ HW5.py
@@ -3,16 +3,13 @@ answer = input("--> ")
 
 
 def find_min_int_out_of_string(some_string):
+    """ A function to find the minimum number outside the list. """
     int_list = list(map(int, some_string.split()))
 
-    print(int_list)
-    for i in range(1, len(int_list)):
+    for i in range(len(int_list)):
         if i not in int_list:
             print(i)
             break
-
-
-
 
 
 find_min_int_out_of_string(answer)
