921ba835bd9d28b9296d7e692420fd0c67cc5d66
Mrlvs <mrlvs2015@gmail.com>
2021-02-14 15:59:02 +0300

    HW7

--- HW7.py
+++ HW7.py
@@ -1,1 +1,11 @@
 
+from collections import Counter
+
+with open("/etc/passwd") as passwdfile:
+        info_list = [(x.strip().split(":"))[-1] for x in passwdfile]
+        shells_dict = Counter(info_list)
+
+
+with open("/etc/group") as groupfile:
+
+
