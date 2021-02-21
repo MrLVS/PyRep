d834701b39c700c7301413582f4223d262407805
Vitaliy Lukin <mrlvs2015@gmail.com>
2021-02-21 18:12:10 +0300

    Функция для поиска наименьшего положительного числа не входящего в заданый список

--- HW5.py
+++ HW5.py
@@ -1,24 +1,20 @@
 print("Введите неотрицательные целые числа, через пробел: ")
 answer = input("--> ")
 
-
 def find_min_int_out_of_string(some_string):
     """ A function to find the minimum number outside the list. """
     int_list = list(map(int, some_string.split()))
 
-    check =[]
-    for i in range(1, len(int_list)):
-        if i in int_list:
-            continue
-        elif i not in int_list:
+    for i in range(1, max(int_list)+1):
+        if i not in int_list:
             print(i)
-            check.append(i)
             break
-
-    if check:
-        pass
-    else:
-        print(len(int_list)+1)
+        elif i not in int_list and i == 1:
+            print(max(int_list)+1)
+            break
+        elif i == max(int_list):
+            print(max(int_list)+1)
+            break
 
 
 find_min_int_out_of_string(answer)
