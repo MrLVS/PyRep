a96aeeb63b39817542804e987e9ee07fc68d3cb5
Vitaliy Lukin <mrlvs2015@gmail.com>
2021-03-23 17:22:08 +0300

    Work with GPS coordinates

--- HW16.py
+++ HW16.py
@@ -2,7 +2,7 @@ import requests
 import urllib
 
 
-API_KEY = "AIzaSyCC8Yge_KE1CoZWmW4-XJgzfpC8G7MLtOw"
+API_KEY = "googleAPIKEY"
 
 with open("C:\HW\Coordinates.txt") as f:
         line = f.readline()
