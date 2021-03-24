31553fcb485d14248baa0fec39d757747fdf603f
Vitaliy Lukin <mrlvs2015@gmail.com>
2021-03-24 14:25:51 +0300

    Work with GPS coordinates

--- HW16.py
+++ HW16.py
@@ -1,33 +1,35 @@
 import requests
 import urllib
 
+API_KEY = ""
 
-API_KEY = "googleAPIKEY"
 
-with open("C:\HW\Coordinates.txt") as f:
+def find_place(filepath):
+    with open(filepath) as f:
         line = f.readline()
         coords = line.strip('()\n')
 
-params = {
-    'latlng': coords,
-    'key': API_KEY,
-    'language': 'ru'
+    params = {
+        'latlng': coords,
+        'key': API_KEY,
+        'language': 'ru'
     }
-maps_params = coords
-url_maps = 'https://www.google.com/maps/search/?api=1&query='
-url_maps_full = url_maps + maps_params
+    maps_params = coords
+    url_maps = 'https://www.google.com/maps/search/?api=1&query='
+    url_maps_full = url_maps + maps_params
 
-url_params = urllib.parse.urlencode(params)
-google_GeoCode_Url = 'https://maps.googleapis.com/maps/api/geocode/json?'
-url = google_GeoCode_Url + url_params
-response = requests.get(url)
-data = response.json()
-if data['status'] == 'OK':
-    result = data['results'][0]
-    location = result['formatted_address']
-    print(location)
-    print(url_maps_full)
-else:
-    print("Произошла ошибка", data['status'])
+    url_params = urllib.parse.urlencode(params)
+    google_geocode_Url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    url = google_geocode_Url + url_params
+    response = requests.get(url)
+    data = response.json()
+    if data['status'] == 'OK':
+        result = data['results'][0]
+        location = result['formatted_address']
+        print(location)
+        print(url_maps_full)
+    else:
+        print("Произошла ошибка", data['status'])
 
 
+find_place("C:\\HW\\Coordinates.txt")
